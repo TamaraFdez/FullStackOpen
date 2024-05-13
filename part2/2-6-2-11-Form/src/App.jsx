0adea7bb103a09c6168602f72a345faea6aa05e6
TamaraFdez <tamarafdezosmi23@gmail.com>
2024-05-13 22:15:03 +0200

    ejercicio 2.11

--- part2/2-6-2-11-Form/src/App.jsx
+++ part2/2-6-2-11-Form/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { FilterContact } from "./components/Filter.jsx";
 import { AddContact } from "./components/AddContact.jsx";
 import { Contactos } from "./components/Contacts.jsx";
+import axios from 'axios'
 import "./App.css";
 
 
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://fullstackopen-4lxg--4001--34455753.local-credentialless.webcontainer.io/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
